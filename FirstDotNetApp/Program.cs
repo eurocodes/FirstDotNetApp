34575725a9982f0595d19e393c71289e34dd8364
eurocodes <emmanuelumeoke@gmail.com>
2021-03-06 10:38:20 +0100

    Added enumeration and other data types

--- FirstDotNetApp/Program.cs
+++ FirstDotNetApp/Program.cs
@@ -4,10 +4,19 @@ namespace FirstDotNetApp
 {
     class Program
     {
+        enum Days { Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday}
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Press any key to continue");
+            Int32 num = 150;
+            double num2 = 250.50;
+            Boolean status = true;
+            String message = "Press any key to continue";
+
+            Console.WriteLine(Days.Sunday);
+            Console.WriteLine(num);
+            Console.WriteLine(num2);
+            Console.WriteLine(status);
+            Console.WriteLine(message);
             Console.ReadKey();
         }
     }
