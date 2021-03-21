5ecc7cdd74111e0ed21b353bf4c336f46e1044ab
eurocodes <emmanuelumeoke@gmail.com>
2021-03-21 14:03:57 +0100

    Learnt about inheritance and polymorphism in c#

--- FirstDotNetApp/Program.cs
+++ FirstDotNetApp/Program.cs
@@ -4,13 +4,22 @@ namespace FirstDotNetApp
 {
     class Program
     {
-        enum Days { Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday}
         static void Main(string[] args)
         {
             String message = "Press any key to continue";
-            
-            Tutorial pTutor = new Tutorial();
+
+            Tutorial initTutor = new Tutorial();
+            Console.WriteLine(initTutor.GetTutorial());
+
+            Guru99Tutorial pTutor = new Guru99Tutorial();
+            pTutor.RenameTutorial(1,".Net by Guru99");
             Console.WriteLine(pTutor.GetTutorial());
+
+            initTutor.SetTutorial(2, "Newform Tutorial");
+            Console.WriteLine(initTutor.GetTutorial());
+
+            initTutor.SetTutorial("Newform Tutorial, Name only");
+            Console.WriteLine(initTutor.GetTutorial());
 
             Console.WriteLine(message);
             Console.ReadKey();
